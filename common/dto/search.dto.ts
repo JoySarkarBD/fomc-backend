c1acd12cd8b162471f571b739ad3658085dd82b0
JoySarkarBD <developer.joysarkar@gmail.com>
2026-02-11 15:29:48 +0600

    chore: "remove the previous boilerplate"

--- common/dto/search.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
-
-export class SearchDto {
-  @IsOptional()
-  @IsString({ message: 'Search query must be a string' })
-  @IsNotEmpty({ message: 'Search query cannot be empty' })
-  search?: string;
-}
--- common/response.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface ServiceResponse<T = any> {
-  success: boolean;
-  message: string;
-  method?: string;
-  endpoint?: string;
-  timestamp: string;
-  statusCode: number;
-  data?: T;
-}
