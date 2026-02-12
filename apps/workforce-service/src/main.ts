27da075c530eb5352810194b431c3aa90301f9bf
JoySarkarBD <developer.joysarkar@gmail.com>
2026-02-12 11:11:50 +0600

    refactor(workforce-service): remove workforce service module and related files

--- apps/workforce-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { WorkforceServiceModule } from "./workforce.module";
-
-async function bootstrap() {
-  const app = await NestFactory.create(WorkforceServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
